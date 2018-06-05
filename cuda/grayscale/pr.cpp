ccff9d2492debcd1c52bd20b762d59f091bd4ab5
Fredy Álvarez <fre.alvarezb@gmail.com>
2018-06-05 14:23:07 -0500

    cuda grayscale

---
+++ cuda/grayscale/pr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+using namespace std;
+
+int main() {
+uchar4 algo;
+}
